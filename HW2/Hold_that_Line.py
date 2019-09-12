e060a6ae7efb0508050aa46bb37cc0362a3b9958
Pupilsch <821058047@qq.com>
2019-09-12 00:21:29 -0500

    Update Hold_that_Line.py

--- HW2/Hold_that_Line.py
+++ HW2/Hold_that_Line.py
@@ -2,9 +2,12 @@
 # map is a 2-D list of size height*width. Each map[i][j] contains a Point (a class defined below)
 # line is a 3-D list of current line. For example[[[0,0],[2,1]],[[2,1],[2,2]]] represent there are two line
 # in the map which is (0,0),(2,1) and (2,1),(2,2)
-#  
+# IMPORTANT!!! For a lineA, lineA[0] should be where it starts(one of two ends) ,and lineA[1] should be where it ends. 
+# endPoint should be a 1*2 list 
 map=[]
 line=[]
+endPoint1=[]
+endPoint2=[]
 
 class Point:
     #ind_x and idx_y is the index of the point in height and width
@@ -38,6 +41,62 @@ def init_map():
     return height,width
 
 def is_twoline_cross(lineA,lineB): #lineA and lineB should be a 2-D list with size 2*2 ([[0,0],[2,1]])
+    x0=lineA[0][0]
+    y0=lineA[0][1]
+    x1=lineA[1][0]
+    y1=lineA[1][1]
+
+    x2=lineB[0][0]
+    y2=lineB[0][1]
+    x3=lineB[1][0]
+    y3=lineB[1][1]
+
+    #assume ax+by+1=0
+
+    a1=(y1-y0)/(y0*x1-y1*x0)
+    b1=(x0-y1)/(y0*x1-y1*x0)
+
+    a2=(y3-y2)/(y2*x3-y3*x2)
+    b2=(x2-y3)/(y2*x3-y3*x2)
+
+    test1=(a1*x2+b1*y2+1)*(a1*x3+b1*y3+1)
+    if test1==0:
+        if (a1*x2+b1*y2+1)==0 & (a1*x3+b1*y3+1)!=0:
+            if ((x2-x0)*(x2-x1))<=0: return True
+        elif (a1*x2+b1*y2+1)!=0 & (a1*x3+b1*y3+1)==0:
+            if ((x3-x0)*(x3-x1))<=0: return True
+        else:
+            if ((x2-x0)*(x2-x1))<=0 or ((x3-x0)*(x3-x1))<=0: return True
+
+    test2=(a2*x0+b2*y0+1)*(a2*x1+b2*y1+1)
+    if test2==0:
+        if (a2*x0+b2*y0+1)==0 & (a2*x1+b2*y1+1)!=0:
+            if ((x0-x2)*(x0-x3))<=0: return True
+        elif (a2*x0+b2*y0+1)!=0 & (a2*x1+b2*y1+1)==0:
+            if ((x1-x2)*(x1-x3))<=0: return True
+        else:
+            if ((x0-x2)*(x0-x3))<=0 or ((x1-x2)*(x1-x3)): return True
+            
+    if test1<0 and test2<0 return True
+    return False
+
+def is_valid_line(point1,point2):
+    lineA=[]
+    lineA.append(point1)
+    lineA.append(point2)
+    line_copy=line
+    
+    for i in line:
+        if i[1]==point1:
+            if (point2[1]-point1[1])/(point2[0]-point1[0]) == -(i[1][1]-i[0][1])/(i[1][0]-i[0][0]): return False
+            line_copy.remove(i)
+            break
+
+    for j in line_copy:
+        if is_twoline_cross(lineA,j): return False
+    
+    return True
+
 
 
 
@@ -47,7 +106,22 @@ def is_valid_input(str):
     bo2=(str[1].isdigit() and str[3].isdigit() and str[7].isdigit() and str[9].isdigit())
     bo3=(str[1]>=0 and str[1]<height and str[7]>=0 and str[7]<height and str[3]>=0 and str[3]<width and str[9]>=0 and str[9]<width)
     if (bo1 and bo2 and bo3) == False: return False
-    bool_cross=is_twoline_cross
+
+    pointA1=[]
+    pointA1.append(int(str[1]))
+    pointA1.append(int(str[3]))
+
+    pointA2=[]
+    pointA2.append(int(str[7]))
+    pointA2.append(int(str[9]))
+
+    lineA=[]
+    lineA.append(pointA1,pointA2)
+
+    if is_valid_line(pointA1,pointA2): return True
+    return False
+
+    
 
 def get_possible_move(map):
     # This fuction has map as input. 
@@ -65,8 +139,10 @@ def choose_move(possible_move):
 
     return pointA,pointB
 
-def ai_move(pointA,pointB):
+def move(pointA,pointB):
     # connect the two point and upgrade the map[], line[]
+    # update endPoint1, endPoint2
+    # update endPoint1, endPoint2
 
 
 if __name__ == "__main__":
@@ -77,12 +153,23 @@ if __name__ == "__main__":
         if is_valid_input(New_Move) == False:
             print("Invalid points input! Please try again.")
             continue
+
+        pointA1=[]
+        pointA1.append(int(New_Move[1]))
+        pointA1.append(int(New_Move[3]))
+
+        pointA2=[]
+        pointA2.append(int(New_Move[7]))
+        pointA2.append(int(New_Move[9]))
+
+        move(pointA1,pointA2)
+
         possible_move=get_possible_move(map)
         if not possible_move:
             print("AI wins!")
             break
         pointA, pointB=choose_move(possible_move)
-        ai_move(pointA,pointB)
+        move(pointA,pointB)
         possible_move=get_possible_move(map)
         if not possible_move:
             print("You wins!")
